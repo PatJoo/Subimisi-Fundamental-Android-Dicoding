db7b0c25700b46f3e099cb3377c177dc544e8a0f
PatJoo <joachimspatrick@gmail.com>
2023-06-28 10:26:50 +0700

    Fixed favorite button

--- app/src/main/java/com/dicoding/mygithub/utils/Result.kt
+++
@@ -1,7 +0,0 @@
-package com.dicoding.mygithub.utils
-
-open class Result {
-    data class Success<out T>(val data: T) : Result()
-    data class Error(val exception: Throwable) : Result()
-    data class Loading(val isLoading: Boolean) : Result()
-}
